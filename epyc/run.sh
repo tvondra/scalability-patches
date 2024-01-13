84031c69c6277d3aae0ebf6d001a235618fd9db4
epyc <epyc>
2024-01-14 00:59:59 +0100

    reduce the scope of tests for full run

--- epyc/run.sh
+++ epyc/run.sh
@@ -6,7 +6,8 @@ MACHINE=epyc
 DBNAME=test
 RUNS=1
 DURATION=15
-CLIENTS="1 2 4 8 16 32 48 64 96 128 160 224 288"
+#CLIENTS="1 2 4 8 16 32 48 64 96 128 160 224 288"
+CLIENTS="1 8 16 32 64 96 160 224"
 PARTITIONS="0 1 10 100 1000"
 BUILDS="3-btscan 1-fstat 4-lock-partitions 2-mempool"
 
@@ -74,6 +75,8 @@ for build in $BUILDS; do
 		./push.sh $MACHINE $OUTDIR
 
 		pg_ctl -D data stop > $OUTDIR/stop.log 2>&1
+
+		./push.sh $MACHINE $OUTDIR
 
 	done
 
