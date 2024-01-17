08f16b14c0acd3d6675f916b1bd4346a93fa3eb3
bench2 <bench2>
2024-01-17 21:00:15 +0100

    update builds

--- xeon/run.sh
+++ xeon/run.sh
@@ -13,7 +13,8 @@ PARTITIONS="0 1 10 100 1000"
 #BUILDS="0-master 5-fastpath 3-btscan 1-fstat 4-lock-partitions 2-mempool"
 #BUILDS="0-master 5-fastpath 3-btscan 1-fstat 4-lock-partitions 2-mempool"
 #BUILDS="6-lock-only"
-BUILDS="7-lock-and-mempool 8-lock-and-mempool-v2"
+#BUILDS="7-lock-and-mempool 8-lock-and-mempool-v2"
+BUILDS="10-lock-and-mempool-rebalance 11-lock-and-mempool-rebalance-fstat 6-lock-only"
 
 PATH_OLD=$PATH
 
