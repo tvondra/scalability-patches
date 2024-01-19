ad2098919421cd9775c480842ffd4b683a908bbc
epyc <epyc>
2024-01-19 17:23:42 +0100

    fix run-index

--- epyc/run-index.sh
+++ epyc/run-index.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+A#!/bin/bash
 
 set -e
 
@@ -32,37 +32,37 @@ for s in 10 100 1000; do
 			createdb $DBNAME >> $OUTDIR/debug.log 2>&1
 
 			# create table with a bunch of columns
-			echo "CREATE TABLE t (id serial primary key" > $OUTDIR/create-$i-$i.sql
+			echo "CREATE TABLE t (id serial primary key" > $OUTDIR/index.create.$p.$i.sql
 
 			# how many columns to create? 100 seems like a nice round value ;-)
 			for c in `seq 1 100`; do
-				echo ", c$c int"  >> $OUTDIR/create-$i-$i.sql
+				echo ", c$c int"  >> $OUTDIR/index.create.$p.$i.sql
 			done
 
-			echo ");" >> $OUTDIR/create-$i-$i.sql
+			echo ");" >> $OUTDIR/index.create.$p.$i.sql
 
 			# now also add some data
-			echo 'insert into t select i' >> $OUTDIR/create-$i-$i.sql
+			echo 'insert into t select i' >> $OUTDIR/index.create.$p.$i.sql
 
 			for c in `seq 1 100`; do
-				echo ", i" >> $OUTDIR/create-$i-$i.sql
+				echo ", i" >> $OUTDIR/index.create.$p.$i.sql
 			done
 
 			# 10k rows per scale sounds about right? pgbench has 100k, but our table is wider
-			echo " from generate_series(1, $s * 10000) s(i);" >> $OUTDIR/create-$i-$i.sql
+			echo " from generate_series(1, $s * 10000) s(i);" >> $OUTDIR/index.create.$p.$i.sql
 
-			echo 'vacuum analyze;' >> $OUTDIR/create-$i-$i.sql
+			echo 'vacuum analyze;' >> $OUTDIR/index.create.$p.$i.sql
 
-			echo 'set max_parallel_maintenance_workers = 8;' >> $OUTDIR/create-$i-$i.sql
+			echo 'set max_parallel_maintenance_workers = 8;' >> $OUTDIR/index.create.$p.$i.sql
 
 			# now create the indexes, spread over all the columns
 			for j in `seq 1 $i`; do
 				# which column to create the index on?
 				c=$((j % 100 + 1))
-				echo "create index on t (c$c);" >> $OUTDIR/create-$i-$i.sql
+				echo "create index on t (c$c);" >> $OUTDIR/index.create.$p.$i.sql
 			done
 
-			psql $DBNAME < $OUTDIR/create-$i-$i.sql > $OUTDIR/debug.log 2>&1
+			psql $DBNAME < $OUTDIR/index.create.$p.$i.sql > $OUTDIR/debug.log 2>&1
 
 			psql $DBNAME -c "vacuum analyze" >> $OUTDIR/debug.log 2>&1
 
