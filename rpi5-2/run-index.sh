b0194fb9a995c40324bd91d434843dfb464e0d63
rpi5-2 <rpi5-2>
2024-01-19 15:49:48 +0000

    fix run-index

--- rpi5-2/run-index.sh
+++ rpi5-2/run-index.sh
@@ -77,7 +77,7 @@ for s in 1 10; do
 		echo "\set aid random(1, 100000 * $s)" > index.sql
 		echo "select * from t where id = :aid;" >> index.sql
 
-		pgbench -n -M prepared -T $((4*DURATION)) -c 32 -j 32 -f index.sql $DBNAME > $OUTDIR/index-warmup-$s-$p.log 2>&1
+		pgbench -n -M prepared -T $((4*DURATION)) -c 32 -j 32 -f index.sql $DBNAME > $OUTDIR/index-warmup-$s-$i.log 2>&1
 
 		for r in $(seq 1 $RUNS); do
 
